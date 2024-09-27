3637160124a352bd03238dde098324abc7cb44b4
alexandermcmillan <mcmillancontact@protonmail.com>
2024-09-27 12:28:07 -0700

    making progress on menu choices

--- cli.py
+++ cli.py
@@ -12,4 +12,36 @@ def main_menu():
     print("6. List books by author")
     print("7. Search for a book by title")
     print("8. Exit")
+
+    choice = input("Select an option")
+
+    if choice == "1":
+      #list all author
+      authors = Author.all_authors()
+      if authors:
+        print("\nAuthors:")
+        for author in authors:
+          print(author)
+
+    if choice == "2":
+      #add new author
+      name = input("Enter the authors name: ")
+      new_author = Author(name)
+      new_author.save()
+      print(f"Author '{name}' added.") 
+
+    if choice == "3":
+      #list all books
+      books = Book.all_books()
+      if books:
+        print("\nBooks:")
+        for book in books:
+          print(book)
+      else:
+        print("No books found.")
+      #Add new book
+      #delete a book
+      #list by author
+      #search by title
+      #exit
   
