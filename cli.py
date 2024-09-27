48169952f15517fdf107d819d32645b954390aba
alexandermcmillan <mcmillancontact@protonmail.com>
2024-09-27 11:47:10 -0700

    cli file created. Initial menu options created

--- cli.py
+++ cli.py
@@ -1,2 +1,15 @@
 from author import Author
-from book import Books
+from book import Book
+
+def main_menu():
+  while True:
+    print("\nLibrary Menu")
+    print("1. List all authors")
+    print("2. Add a new author")
+    print("3. List all books")
+    print("4. Add a new book")
+    print("5. Delete a book")
+    print("6. List books by author")
+    print("7. Search for a book by title")
+    print("8. Exit")
+  
